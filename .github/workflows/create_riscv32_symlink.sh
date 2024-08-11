1078d35c342986f498e7a24041e97aeba4c6b23b
HalfSweet <halfsweet@halfsweet.cn>
2024-08-11 23:36:23 +0800

    fix: 符号链接

--- .github/workflows/create_riscv32_symlink.sh
+++ .github/workflows/create_riscv32_symlink.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd /home/runner/work/_temp/.setup-riscv-gnu-toolchain/bin/
+old_path="/home/runner/work/_temp/.setup-riscv-gnu-toolchain/bin"
 
 new_path="/home/runner/risv32-unknown-elf/bin"
 mkdir -p $new_path
+
+cd $old_path
 
 # 查找所有以 "riscv-none-elf-" 开头的文件
 for file in riscv-none-elf-*; do
@@ -13,7 +15,7 @@ for file in riscv-none-elf-*; do
         new_link="$new_path/riscv32-unknown-elf-${file#riscv-none-elf-}"
         
         # 创建软链接
-        ln -s "$file" "$new_link"
+        ln -s "$old_path/$file" "$new_link"
 
         printf "Create symlink: %s -> %s\n" "$new_link" "$file"
     fi
